9757c106a30b610b41a2af131e53542f4733ea7d
Neha Saggam <nehasaggam@gmail.com>
2019-05-16 23:32:14 +0530

    Resolve security issues

--- queryStringToJSON.js
+++ queryStringToJSON.js
@@ -1,3 +1,20 @@
+function isArray(key) {
+    return key.endsWith("[]");
+}
+
+function isValid(queryParam) {
+    return queryParam && queryParam.trim().length;
+}
+
+function getKeyValuePair(queryParam) {
+    const param = queryParam.split("=");
+    return { key: param[0].trim(), value: param[1] };
+}
+
+function isArrayPresent(params, array) {
+   return params.hasOwnProperty(array)
+}
+
 const queryStringToJSON = (queryString) => {
     const queryParams = queryString.split("&");
     const params = {};
@@ -19,20 +36,3 @@ const queryStringToJSON = (queryString) => {
 }
 
 export default queryStringToJSON;
-
-function isArray(key) {
-    return key.endsWith("[]");
-}
-
-function isValid(queryParam) {
-    return queryParam && queryParam.trim().length;
-}
-
-function getKeyValuePair(queryParam) {
-    const param = queryParam.split("=");
-    return { key: param[0].trim(), value: param[1] };
-}
-
-function isArrayPresent(params, array) {
-   return params.hasOwnProperty(array)
-}
